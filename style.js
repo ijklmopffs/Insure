92a5cac29838e174339873719f22beb1cd84d321
Nuel Elekwachi <elekwaeke300@gmail.com>
2023-05-03 17:10:22 +0100

    First commit

---
+++ style.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".links");
+
+hamburger.addEventListener("click", function () {
+  menu.classList.toggle("active");
+  if (menu.classList.contains("active")) {
+    hamburger.setAttribute("src", "./images/icon-close.svg");
+  } else {
+    hamburger.setAttribute("src", "./images/icon-hamburger.svg");
+  }
+  console.log("clicked");
+});
+
+// menuIcon.addEventListener("click", function () {});
