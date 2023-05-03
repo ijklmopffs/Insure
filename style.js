e7dd1c21ec91c7d44cb9f837fb8daa982e6d4f43
Nuel Elekwachi <elekwaeke300@gmail.com>
2023-05-03 22:38:49 +0100

    update

--- style.js
+++ style.js
@@ -1,14 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
-const menu = document.querySelector(".links");
+const menu = document.querySelector(".mobile_links");
 
 hamburger.addEventListener("click", function () {
   menu.classList.toggle("active");
   if (menu.classList.contains("active")) {
+    menu.style.display = "flex";
     hamburger.setAttribute("src", "./images/icon-close.svg");
   } else {
+    menu.style.display = "none";
     hamburger.setAttribute("src", "./images/icon-hamburger.svg");
   }
-  console.log("clicked");
 });
-
-// menuIcon.addEventListener("click", function () {});
